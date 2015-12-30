cb0a6dadddcfebd8515f8a1ef4bcb6cfa99df2f0
Massimo Zambelli <mass.zambelli@gmail.com>
2015-12-30 02:26:04 +0100

    Parser as an object.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+	var Parser = new BLParser();
 	function showSetItem(item){
 		var dom = '<div class="setitem">' +
 					'<h3>' + item.name + '</h3>' +
@@ -11,15 +11,15 @@ $(document).ready(function(){
 	$('#launchsearch').click(function(){
 		var setid = $('#setid').val();
 
-		getBLsetInfo(setid, function(ok, blid){
+		Parser.getBLsetInfo(setid, function(ok, blid){
 			console.log(ok + blid);
 			if(ok){
-				getInventoryFromSet(blid, function(ok, inventory){
+				Parser.getInventoryFromSet(blid, function(ok, inventory){
 					if(ok){
 						for(var i =0; i<inventory.length;i++){
 							//for each item we show it, and we get pricing
 							showSetItem(inventory[i]);
-							getItemPriceForQty(inventory[i], function(ok, value){
+							Parser.getItemPriceForQty(inventory[i], function(ok, value){
 								if(ok){
 									//update info with price
 								}
